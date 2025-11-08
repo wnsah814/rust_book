5a6557e3f59e90f12c442847a820e57c657fb302
owunr <sjmskm@gmail.com>
2025-11-08 23:36:11 +0900

    3.4 comments

---
+++ chap03/comments/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+
+    // hello, world
+
+    // So we're doing something complicated here, long enough that we need
+    // multiple lines of comments to do it! Whew! Hopefully, this comment will
+    // explain what's going on.
+
+    let lucky_number = 7; // I'm feeling lucky today!
+
+    // I'm feeling lucky today
+    let lucky_number = 7;
+}
