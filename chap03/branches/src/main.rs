c1089542788855b99f204f1e3b75b03dc27f7a71
owunr <sjmskm@gmail.com>
2025-11-09 14:07:58 +0900

    3.5 control flow

---
+++ chap03/branches/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let x = 3;
+    if x < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    // condition must be a boolean
+    let number = 3;
+    // if number {
+    //     println!("y was three");
+    // }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    let number = 6;
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {}", number);
+
+    // let number = if condition { 5 } else { "six" }; // error: if and else have incompatible types
+}
