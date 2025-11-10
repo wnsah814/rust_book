604b2d0d90aa84d33bc7fc5ecaa3383ef1cbe83c
wnsah814 <sjmskm@gmail.com>
2025-11-10 22:31:29 +0900

    7.3 paths for referring to an item in the module tree

--- chap07/restaurant/src/lib.rs
+++ chap07/restaurant/src/lib.rs
@@ -1,8 +1,8 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
-        fn sear_at_table() {}
+        fn seat_at_table() {}
     }
 
     mod serving {
@@ -12,4 +12,52 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    // crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    // front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // Error! seasonal_fruit is private
+    // meal.seasonal_fruit = String::from("blueberries"); 
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
 }
