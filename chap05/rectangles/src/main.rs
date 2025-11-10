f2a48cafd69657fdb65699d8bd0728fbdf367b08
wnsah814 <sjmskm@gmail.com>
2025-11-10 14:39:08 +0900

    5.2 an example program using structs

---
+++ chap05/rectangles/src/main.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+
+    println!("rect1 is {rect1:#?}");
+
+    let scale = 2;
+    let rect2 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect2);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
