559cd8c68154990b8daebbad7098246054f824fd
owunr <sjmskm@gmail.com>
2025-11-12 17:22:34 +0900

    8.1 sorting lists of values with vectors

---
+++ chap08/vector/src/main.rs
@@ -0,0 +1,51 @@
+fn main() {
+    let _v: Vec<i32> = Vec::new();
+
+    let _v = vec![1, 2, 3];
+
+    let mut v = Vec::new();
+
+    v.push(5);
+    v.push(6);
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}.");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}."),
+        None => println!("There is no third element."),
+    }
+
+    // let does_not_exist = &v[100]; // crash
+    let does_not_exist = v.get(100); // None
+
+    let mut v = vec![1, 2, 3, 4, 5];
+    let first = &v[0];
+    // v.push(6); // error: cannot borrow `v` as mutable because it is also borrowed as immutable
+    println!("The first element is: {first}");
+
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{i}");
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+}
